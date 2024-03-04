c05fdddf48b4573619488aeab776b6e7659f9dd6
Pedro Henrique <prdrinho13@gmail.com>
2024-03-04 17:05:39 -0300

    docs: :memo: rascunho / inicialização do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <TemplateHelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TemplateHelloWorld from "./components/template/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TemplateHelloWorld,
+  },
+};
 </script>
 
 <style>
